c488b08a9b3e2770cdb60d1be5ed74393e4f967f
Amanda Zheng <azheng00@stuy.edu>
2019-12-03 16:15:32 -0500

    started ls function

---
+++ shell.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <dirent.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+void ls(char * args);
