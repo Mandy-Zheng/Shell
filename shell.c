2e36ca442521c5270fc8bfec1c11581bc1aa6ac1
Amanda Zheng <azheng00@stuy.edu>
2019-12-04 15:08:36 -0500

    fixed ls

--- shell.c
+++ shell.c
@@ -8,26 +8,28 @@ void ls(char * args){
   int size=0;
   char directories[1000]="";
   char files[1000]="";
-  if (opened!= NULL){
-    struct stat readfile;
-    char newpath[2000]="";
-    strcpy(newpath, path);
-    strcat(newpath,"/");
-    strcat(newpath,read->d_name);
-    int items=stat(newpath, &readfile);
-    if(items==-1){
-        printf("Error: %s\n",strerror(errno));
-      }else{
-        if((readfile.st_mode & S_IFMT) == S_IFDIR){
-          strcat(directories,"\n\t");
-          strcat(directories,read->d_name);
+  if(opened!=NULL){
+      struct dirent * read=readdir(opened);
+      while(read != NULL){
+        struct stat readfile;
+        char newpath[2000]="";
+        strcpy(newpath,path);
+        strcat(newpath,"/");
+        strcat(newpath,read->d_name);
+        int items=stat(newpath, &readfile);
+        if(items==-1){
+          printf("Error: %s\n",strerror(errno));
         }else{
-          strcat(files,"\n\t");
-          strcat(files,read->d_name);
+          if((readfile.st_mode & S_IFMT) == S_IFDIR){
+            strcat(directories,"\n\t");
+            strcat(directories,read->d_name);
+          }else{
+            strcat(files,"\n\t");
+            strcat(files,read->d_name);
+          }
         }
+        size+=readfile.st_size;
+        //printf("\n %s: %d", );
+        read=readdir(opened);
       }
-      size+=readfile.st_size;
-      //printf("\n %s: %d", );
-      read=readdir(opened);
-  }
 }
