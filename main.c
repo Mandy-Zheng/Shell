462bb61d7a7f53507ded200e0bd7a154a1549889
Amanda Zheng <azheng00@stuy.edu>
2019-12-04 23:46:24 -0500

    fixing newline problems

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main(int argc, char const *argv[]) {
   char * args=calloc(sizeof(char),1000);
   fgets(args, 1000, stdin);
+  int argslen=strlen(args);
+  args[argslen-1]='\0';
   printf("%s\n",args);
   while(strcmp(args,"exit\n")!=0){
     char ** command=parse(args);
@@ -16,6 +18,8 @@ int main(int argc, char const *argv[]) {
       executing(command);
     }
     fgets(args, 1000, stdin);
+    argslen=strlen(args);
+    args[argslen-1]='\0';
   }
   return 0;
 }
