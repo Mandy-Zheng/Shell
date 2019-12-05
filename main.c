4879608746071e34de653fdd35d3c088921b4abe
josephlee <jlee01@stuy.edu>
2019-12-05 15:42:13 -0500

    took care of mistakes

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 
 int main(int argc, char const *argv[]) {
   char * args=calloc(sizeof(char),1000);
-  args="dummy";
   while(strcmp(args,"exit")!=0){
-    printf("%s\n","/Shell$" );
+    printf("/Shell$ " );
     fgets(args, 1000, stdin);
     args[strlen(args)-1]='\0';
     char ** command=parse(args);
+    printf("%d\n",lengthArgs(command));
     if(lengthArgs(command)>0){
-      int i=0;
       executing(command);
     }
   }
--- shell.c
+++ shell.c
@@ -2,7 +2,7 @@
 int lengthArgs(char **command){
   int i=0;
   int length=0;
-  while(strcmp(command[i],"\0")!=0){
+  while(command[i]!=NULL){
     length++;
     i++;
   }
@@ -10,19 +10,13 @@ int lengthArgs(char **command){
 }
 char ** parse(char * args){
   char ** command=calloc(sizeof(char *),100);
-  char * part=strsep(&args, " ");
-  command[0]=part;
-  int i=1;
-  part=strsep(&args, " ");
-  while (part!=NULL){
-    command[i]=part;
-    i++;
-    part=strsep(&args," ");
+  char * part=args;
+  for (size_t i = 0; part!=NULL; i++) {
+    command[i]= strsep(&part," ");
   }
-  command[i]="\0";
   return command;
 }
-void executing(char ** command){ 
+void executing(char ** command){
   if(fork()==0){
     execvp(command[0],command);
   }else{
