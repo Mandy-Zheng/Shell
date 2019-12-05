f64a26579643099d5b44502f21eda2d40efcc1e6
Amanda Zheng <azheng00@stuy.edu>
2019-12-04 22:13:56 -0500

    callocing

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 int main(int argc, char const *argv[]) {
-  char args[1000];
+  char args[]=calloc(sizeof(char),1000);
   fgets(args, 1000, stdin);
   while(strcmp(args,"exit\n")!=0){
     if(lengthArgs(args)>0){
