194ca3d93a7f0edcfb0d03a165a023d82ab6fab2
Amanda Zheng <azheng00@stuy.edu>
2019-12-09 15:28:44 -0500

    making colors

--- main.c
+++ main.c
@@ -4,12 +4,13 @@ int main(int argc, char const *argv[]) {
   char * args=calloc(sizeof(char),1000);
   while(strcmp(args,"exit")!=0){
     printf("/Shell$ " );
+    enum color=DarkRed;
     fgets(args, 1000, stdin);
     args[strlen(args)-1]='\0';
     char ** commandmulti=parseMulti(args);
     printf("%s\n",commandmulti[0]);
     if(lengthArgs(commandmulti)>0){
-      set_color(DarkRed);
+      set_color(color);
       for (size_t i = 0; i < lengthArgs(commandmulti); i++) {
         char ** command = parse(commandmulti[i]);
         if(!isRedirect(commandmulti[i]) &&
