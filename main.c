1f2b504de1351959762a2eb7a18cd70442e6312d
Amanda Zheng <azheng00@stuy.edu>
2019-12-10 15:30:54 -0500

    finished merging

--- main.c
+++ main.c
@@ -4,19 +4,20 @@ int main(int argc, char const *argv[]) {
   char * args=calloc(sizeof(char),1000);
   char dir_path[512];
   while(strcmp(args,"exit")!=0){
-
-    printf("/Shell$ " );
-    enum color c=DarkRed; 
-    fgets(args, 1000, stdin); 
+    getcwd(dir_path,sizeof(dir_path));
+    printf("%s/Shell$ ",dir_path);
+    enum color c=DarkRed;
+    fgets(args, 1000, stdin);
     args[strlen(args)-1]='\0';
-    char ** commandmulti=parseMulti(args); 
+    char ** commandmulti=parseMulti(args);
+
      // set_color(c);
     int x=getchar();
     printf("\n[%d]\n",x);
     if(x==27){
       printf("yes");
       x=getchar();
-      printf("\n[%d]\n",x);	
+      printf("\n[%d]\n",x);
       if(x==91){
         printf("yes2");
 	x=getchar();
@@ -27,13 +28,6 @@ int main(int argc, char const *argv[]) {
        	}
       }
     }
-
-    getcwd(dir_path,sizeof(dir_path));
-    printf("%s/Shell$ ",dir_path);
-    enum color c=DarkRed;
-    fgets(args, 1000, stdin);
-    args[strlen(args)-1]='\0';
-    char ** commandmulti=parseMulti(args);
 
     if(lengthArgs(commandmulti)>0){
 
