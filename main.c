eae813466b24c5fb984272caf71b25b04be22d46
josephlee <jlee01@stuy.edu>
2019-12-10 15:28:31 -0500

    fixing arrows stuff

--- main.c
+++ main.c
@@ -4,17 +4,33 @@ int main(int argc, char const *argv[]) {
   char * args=calloc(sizeof(char),1000);
   while(strcmp(args,"exit")!=0){
     printf("/Shell$ " );
-    enum color c=DarkRed;
-    fgets(args, 1000, stdin);
+    enum color c=DarkRed; 
+    fgets(args, 1000, stdin); 
     args[strlen(args)-1]='\0';
-    char ** commandmulti=parseMulti(args);
-    printf("%s\n",commandmulti[0]);
+    char ** commandmulti=parseMulti(args); 
+     // set_color(c);
+    int x=getchar();
+    printf("\n[%d]\n",x);
+    if(x==27){
+      printf("yes");
+      x=getchar();
+      printf("\n[%d]\n",x);	
+      if(x==91){
+        printf("yes2");
+	x=getchar();
+	printf("\n[%d]\n",x);
+        if(x==68){
+	  printf("\nyes3");
+          printf("\033[D");
+       	}
+      }
+    }
     if(lengthArgs(commandmulti)>0){
-      set_color(c);
-      for (size_t i = 0; i < lengthArgs(commandmulti); i++) {
+
+      for (size_t i =0 ; i< lengthArgs(commandmulti);i++){
         char ** command = parse(commandmulti[i]);
         if(!isRedirect(commandmulti[i]) &&
-           !isChangeDirectory(command)){
+           !isPipe(command)){
              executing(command);
 	      }
       }
