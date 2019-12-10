ba60873f780e63cca190879ebf9d12f95f6ee64e
josephlee <jlee01@stuy.edu>
2019-12-10 16:28:27 -0500

    done with colors

--- main.c
+++ main.c
@@ -4,31 +4,13 @@ int main(int argc, char const *argv[]) {
   char * args=calloc(sizeof(char),1000);
   char dir_path[512];
   while(strcmp(args,"exit")!=0){
+    set_color(Cyan);
     getcwd(dir_path,sizeof(dir_path));
-    printf("%s/Shell$ ",dir_path);
-    enum color c=DarkRed;
+    printf("%s$ ",dir_path);
+    set_color(White);
     fgets(args, 1000, stdin);
     args[strlen(args)-1]='\0';
     char ** commandmulti=parseMulti(args);
-
-     // set_color(c);
-    int x=getchar();
-    printf("\n[%d]\n",x);
-    if(x==27){
-      printf("yes");
-      x=getchar();
-      printf("\n[%d]\n",x);
-      if(x==91){
-        printf("yes2");
-	x=getchar();
-	printf("\n[%d]\n",x);
-        if(x==68){
-	  printf("\nyes3");
-          printf("\033[D");
-       	}
-      }
-    }
-
     if(lengthArgs(commandmulti)>0){
 
       for (size_t i =0 ; i< lengthArgs(commandmulti);i++){
