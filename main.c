8f4da0e139123b143267c7457d057d7ae9567d94
Amanda Zheng <azheng00@stuy.edu>
2019-12-11 00:55:11 -0500

    stripping spaces

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ int main(int argc, char const *argv[]) {
       if(lengthArgs(commandmulti)>0){
 
         for (size_t i =0 ; i< lengthArgs(commandmulti);i++){
+          strip(commandmulti[i],' ');
           if(strlen(commandmulti[i])>0){
             char ** command = parse(commandmulti[i]);
             if(!isRedirect(commandmulti[i]) &&
